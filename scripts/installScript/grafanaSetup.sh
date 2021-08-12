49fd52d320e7c74d4113cfc570240e8dd1247140
Niels Korschinsky <niels.korschinsky@gmail.com>
2021-08-12 14:27:59 +0200

    Update grafanaSetup.sh

--- scripts/installScript/grafanaSetup.sh
+++ scripts/installScript/grafanaSetup.sh
@@ -106,8 +106,6 @@ EOF
         if generate_cert "$httpsKeyPath" "$httpsCertPath" httpsKeyPath httpsCertPath ; then
             unsafeSsl=true
         fi
-        echo "key: ${httpsKeyPath}"
-        echo "cert: ${httpsCertPath}"
 
         checkReturn sudo chown -R grafana:grafana "${httpsKeyPath}"
         checkReturn sudo chown -R grafana:grafana "${httpsCertPath}"
