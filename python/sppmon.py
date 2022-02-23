bb0cff0d710d1a6b47141b09c62311929a14f1c6
Niels Korschinsky <niels.korschinsky@gmail.com>
2022-02-23 16:53:16 +0100

    sppmon version increase

--- python/sppmon.py
+++ python/sppmon.py
@@ -80,6 +80,7 @@ Author:
  08/27/2021 version 1.0.1  Reverted parts of the SLA-Endpoint change
  08/31/2021 version 1.0.2  Changed VADP table definition to prevent drop of false duplicates
  09/09/2021 version 1.1.0  Increase logging for REST-API errors, add ssh-client skip option for cfg file.
+ 02/22/2021 version 1.1.1  Only ssh-calls the vSnap-api if it is available
 """
 from __future__ import annotations
 
@@ -108,7 +109,7 @@ from utils.methods_utils import MethodUtils
 from utils.spp_utils import SppUtils
 
 # Version:
-VERSION = "1.1.0  (2021/09/09)"
+VERSION = "1.1.1  (2021/02/22)"
 
 
 # ----------------------------------------------------------------------------
