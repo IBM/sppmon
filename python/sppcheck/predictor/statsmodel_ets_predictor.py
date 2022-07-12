25c3d46f6674cec94bf09c809228caa0c1149a5a
Niels Korschinsky <niels.korschinsky@gmail.com>
2022-07-12 02:15:27 +0200

    comments to the predictor and spacing of api

--- python/sppcheck/predictor/statsmodel_ets_predictor.py
+++ python/sppcheck/predictor/statsmodel_ets_predictor.py
@@ -77,29 +77,34 @@ class StatsmodelEtsPredictor(PredictorInterface):
                     data_series: Series,
                     forecast_years: float) -> Series:
 
-        # convert to hour
+
+        # read the frequency to calculate how many data points needs to be forecasted
         try:
+            # convert to hour
             dp_freq_hour: float = data_series.index.freq.nanos / 3600000000000 # type: ignore
         except AttributeError as error:
             ExceptionUtils.exception_info(error)
             raise ValueError("The data series is corrupted, no frequency at the index available", data_series.index)
 
+        forecast_dp_count = round((forecast_years * 365 * 24) / dp_freq_hour)
+
+
          # discard prediction data without new data
         hours_last_data = (datetime.now() - data_series.index.max()).total_seconds() / (60 * 60)
 
         # discard if the data is older than 7 days and 3 times the frequency
-        # May some datapoints fail, therefore this grace period
+        # May some data points fail, therefore this grace period
         if hours_last_data > 24 * 7 and hours_last_data > dp_freq_hour * 3:
             raise ValueError("This set of data is too old to be used")
 
-        forecast_dp_count = round((forecast_years * 365 * 24) / dp_freq_hour)
-
         LOGGER.debug(f"forecasting using {len(data_series)} datapoints")
 
+        # without enough values the forecast wont work
         if len(data_series) < 15:
             raise ValueError(f"At least 15 values are required for a prediction, only {len(data_series)} given", data_series)
 
         # interpolate nan values
+        # this is a last resort interpolate, split to allow previous summary between the preparation and forecast to work on nan values
         nan_count = len(data_series) - data_series.count()
         if nan_count:
             LOGGER.info(f"{nan_count} values are nan when predicting, forcing an interpolation")
