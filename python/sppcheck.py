bbf5d0e2921a4dc56847c29472a63ca722bd5bbd
Niels Korschinsky <niels.korschinsky@gmail.com>
2022-08-21 01:51:56 +0200

    Release version change.

--- python/sppcheck.py
+++ python/sppcheck.py
@@ -46,7 +46,7 @@ from utils.spp_utils import SppUtils
 from utils.sppcheck_utils import Themes
 
 # Version:
-VERSION = "0.7  (2022/08/03)"
+VERSION = "1.0  (2022/08/21)"
 
 
 # ----------------------------------------------------------------------------
